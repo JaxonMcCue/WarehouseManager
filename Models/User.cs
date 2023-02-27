5fe3a1ee53c5cc1df4dfe04b3e3e1cbf5820e6f5
Anthony Schaefbauer <as745677@southeast.edu>
2023-02-27 14:50:41 -0600

    user, role class

---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WarehouseManager.Models
+{
+    public class User : IdentityUser
+    {
+        public int CustomerID { get; set; }
+
+        public Customer customer { get; set; }
+
+        [NotMapped]
+        public IList<String> RoleNames { get; set; }
+
+        public User() { }
+
+    }
+}

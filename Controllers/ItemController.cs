42993eb3e05cbd4e22a87457ffdd18a28e2f280c
Jaxon <vm737839@LR002D07>
2023-03-01 14:48:07 -0600

    Delete Item

--- Controllers/ItemController.cs
+++ Controllers/ItemController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -40,6 +41,33 @@ namespace WarehouseManager.Controllers
                 return RedirectToAction(nameof(DisplayItems));
             }
             return View(item);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DeleteItem(int id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _context.Items.FirstOrDefaultAsync(i => i.ItemID == id);
+            if(item == null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+        }
+
+        [HttpPost, ActionName("DeleteItem")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteItemConfirmed(int id)
+        {
+            var item = await _context.Items.FindAsync(id);
+            _context.Items.Remove(item);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(DisplayItems));
         }
     }
 }

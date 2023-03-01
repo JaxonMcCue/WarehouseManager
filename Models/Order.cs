a858a93fb13f870720cff3bd2e00691012bf5c85
Caden Moore <cm740790@southeast.edu>
2023-03-01 13:09:00 -0600

    Order and OrderItem classes

---
+++ Models/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WarehouseManager.Models
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public int CustomerID { get; set; }
+        public decimal OrderCost { get; set; }
+        public bool Completed { get; set; }
+        public bool Cancelled { get; set; }
+
+        public Order() { }
+
+        public Order(decimal orderCost)
+        {
+            OrderCost = orderCost;
+            Completed = false;
+            Cancelled = false;
+        }
+    }
+}
---
+++ Models/OrderItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WarehouseManager.Models
+{
+    public class OrderItem
+    {
+        public int ItemID { get; set; }
+        public int OrderID { get; set; }
+    }
+}

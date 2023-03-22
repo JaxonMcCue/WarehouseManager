b30d4b3661be3e95390a081681ee930836a1aa30
Anthony Schaefbauer <as745677@southeast.edu>
2023-03-22 16:23:03 -0500

    Added customer controller, customer contact page, search customer function

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WarehouseManager.Data;
+using WarehouseManager.Models;
+using WarehouseManager.Models.ViewModels;
+
+namespace WarehouseManager.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var customers = _context.Customers.Join(_context.Users, c => c.CustomerID, u => u.CustomerID,
+                (c, u) => new CustomerViewModel
+                {
+                    CustomerID = c.CustomerID,
+                    BusinessName = c.BusinessName,
+                    FirstName = c.FirstName,
+                    LastName = c.LastName,
+                    Address = c.Address,
+                    City = c.City,
+                    State = c.State,
+                    Zip = c.Zip,
+                    Phone = c.Phone,
+                    Email = u.Email
+                }).ToList();
+
+            return View(customers);
+
+        }
+
+        public IActionResult Search(string searchString)
+        {
+            var customers = _context.Customers
+            .Join(_context.Users, c => c.CustomerID, u => u.CustomerID,
+            (c, u) => new CustomerViewModel
+            {
+                CustomerID = c.CustomerID,
+                BusinessName = c.BusinessName,
+                FirstName = c.FirstName,
+                LastName = c.LastName,
+                Address = c.Address,
+                City = c.City,
+                State = c.State,
+                Zip = c.Zip,
+                Phone = c.Phone,
+                Email = u.Email
+            });
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                customers = customers.Where(c => c.BusinessName.Contains(searchString) ||
+                                            c.FirstName.Contains(searchString) ||
+                                            c.LastName.Contains(searchString) ||
+                                            c.City.Equals(searchString) ||
+                                            c.State.Equals(searchString));
+            }
+            return View("Index", customers.ToList());
+        }
+    }
+}
---
+++ Models/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WarehouseManager.Models.ViewModels
+{
+    public class CustomerViewModel
+    {
+        public int CustomerID { get; set; }
+        [DisplayName("Business Name")]
+        public string BusinessName { get; set; }
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}

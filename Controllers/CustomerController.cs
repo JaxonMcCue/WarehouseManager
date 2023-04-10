e5f6232a42fdb332fb2542797272353f20306f7b
JaxonMcCue <jaxonmccue718@gmail.com>
2023-04-10 14:18:55 -0500

    Customer controller blocked by regular users

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -11,6 +12,7 @@ using WarehouseManager.Models.ViewModels;
 
 namespace WarehouseManager.Controllers
 {
+    [Authorize(Roles = "Admin,Sales")]
     public class CustomerController : Controller
     {
         private readonly ApplicationDbContext _context;
